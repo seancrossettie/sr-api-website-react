becb2a673eb9d6ae37a9973b126e7c587f7057e5
Sean <jb@JBs-MacBook-Pro.local>
2021-04-22 19:34:05 -0500

    saved changes

--- src/App/index.js
+++ src/App/index.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Button } from 'reactstrap';
+import {
+  Button, Col, Row, Form, FormGroup, Label, Input
+} from 'reactstrap';
 import getJokes from '../helpers/data/jokeData';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -37,6 +39,38 @@ function App() {
       <Button color="info" onClick={handleClick}>
         {showPunchLine ? 'Get Another Joke' : 'Get Punchline'}
       </Button>
+      <div className="m-5">
+        <h1>Lyrics Generator</h1>
+        <Form>
+          <Row form>
+            <Col md={6}>
+              <FormGroup>
+                <Label for="artistName">Artist</Label>
+                <Input
+                  type="text"
+                  name="artistName"
+                  id="artistName"
+                  placeholder="Artist"
+                  />
+              </FormGroup>
+            </Col>
+            <Col md={6}>
+              <FormGroup>
+                <Label for="songName">Song</Label>
+                <Input
+                  type="text"
+                  name="songName"
+                  id="songName"
+                  placeholder="Song"
+                  />
+              </FormGroup>
+            </Col>
+          </Row>
+        </Form>
+        <Button color="info" onClick={lyricsHandleClick}>
+          {showLyrics ? 'Get More Lyrics' : 'Find a Song'}
+        </Button>
+      </div>
     </div>
   );
 }
