6565091db81aef67fad6e1ae128995854ab77d66
Sean <jb@JBs-MacBook-Pro.local>
2021-04-28 14:41:04 -0500

    added router --- Do not merge

---
+++ src/views/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/index.scss';
+
+export default function Home() {
+  return (
+    <>
+    <h1>Home Page</h1>
+    </>
+  );
+}
---
+++ src/views/Weather.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import WeatherForm from '../components/WeatherForm';
+
+const Weather = () => (
+    <>
+      <WeatherForm />
+    </>
+);
+
+export default Weather;
